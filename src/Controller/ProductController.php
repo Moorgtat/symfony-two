dccfec9114f774c3a5fc256e27e57478f97eb474
Moorgtat <mat.lecardinal@gmail.com>
2020-07-23 18:05:10 +0200

    Routing homepage / List product / Design show and list

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/products", name="products_index")
+     */
+    public function index(ProductRepository $repo)
+    {
+        $products = $repo->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+     /**
+     * @Route("/product/{slug}", name="product_show")
+     * @return Response
+     */
+    public function show(Product $product)
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,40 +10,40 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        $faker = Factory::create('FR-fr'); 
+  public function load(ObjectManager $manager)
+  {
+    $faker = Factory::create('FR-fr');
 
-        for($i = 1; $i <= 30; $i++) {
+    for ($i = 1; $i <= 30; $i++) {
 
-          $product = new Product();
+      $product = new Product();
 
-          $title = $faker->sentence();
-          $coverImage = $faker->imageUrl(450,200);
-          $introduction = $faker->paragraph(2);
-          $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
-        
-          $product->setTitle($title)
-                  ->setCoverImage($coverImage)
-                  ->setIntroduction($introduction)
-                  ->setContent($content)
-                  ->setPrice(mt_rand(39, 289))
-                  ->setRooms(mt_rand(1, 7)); 
+      $title = $faker->sentence();
+      $coverImage = $faker->imageUrl(450, 200);
+      $introduction = $faker->paragraph(2);
+      $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
 
-                  for($j = 1; $j <= mt_rand(2, 5); $j++) {
-                
-                     $image = new Image();
+      $product->setTitle($title)
+              ->setCoverImage($coverImage)
+              ->setIntroduction($introduction)
+              ->setContent($content)
+              ->setPrice(mt_rand(39, 289))
+              ->setRooms(mt_rand(1, 7));
 
-                     $image->setUrl($faker->imageUrl())
-                           ->setCaption($faker->sentence())
-                           ->setProduct($product);
-                     $manager->persist($image);
-                  }
+      for ($j = 1; $j <= mt_rand(2, 5); $j++) {
 
-          $manager->persist($product);
+        $image = new Image();
 
-        }
+        $image->setUrl($faker->imageUrl())
+              ->setCaption($faker->sentence())
+              ->setProduct($product);
+        $manager->persist($image);
+      }
 
-        $manager->flush();
+      $manager->persist($product);
+
     }
+
+    $manager->flush();
+  }
 }
